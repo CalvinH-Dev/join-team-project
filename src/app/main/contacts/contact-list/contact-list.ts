57a8303817fab2080246890aac8b23e4ab1fdaaf
Madina Askar Zada <52121682+madina-aska@users.noreply.github.com>
2025-09-21 20:34:58 +0200

    Add routing to ContactView and make it dynamic based on URL

--- src/app/main/contacts/contact-list/contact-list.ts
+++ src/app/main/contacts/contact-list/contact-list.ts
@@ -1,6 +1,7 @@
 import { KeyValuePipe } from "@angular/common";
 import { Component, inject, output } from "@angular/core";
 import { Firestore } from "@angular/fire/firestore";
+import { Router } from "@angular/router";
 import { ContactService } from "app/core/services/contact-service";
 
 @Component({
@@ -12,11 +13,12 @@ import { ContactService } from "app/core/services/contact-service";
 export class ContactList {
 	db = inject(Firestore);
 	contactService = inject(ContactService);
+	router = inject(Router);
 
 	contactSelected = output<string>();
 
 	onContactSelect(id: string | undefined) {
 		if (!id) return;
-		this.contactSelected.emit(id);
+		this.router.navigate(["/contacts", id]);
 	}
 }
--- src/app/main/contacts/contact-view/contact-view.ts
+++ src/app/main/contacts/contact-view/contact-view.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { Firestore } from "@angular/fire/firestore";
+import { ActivatedRoute } from "@angular/router";
 import { ContactService } from "app/core/services/contact-service";
 
 @Component({
@@ -7,11 +8,17 @@ import { ContactService } from "app/core/services/contact-service";
 	templateUrl: "./contact-view.html",
 	styleUrls: ["./contact-view.scss"],
 })
-export class ContactView {
+export class ContactView implements OnInit {
 	firestore = inject(Firestore);
 	contactService = inject(ContactService);
+	route = inject(ActivatedRoute);
 
-	constructor() {
-		this.contactService.getDocumentById("1pBp6LKabOGB5sN1A4D6");
+	ngOnInit(): void {
+		this.route.paramMap.subscribe((params) => {
+			const id = params.get("id");
+			if (id) {
+				this.contactService.getDocumentById(id);
+			}
+		});
 	}
 }

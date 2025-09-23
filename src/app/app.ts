57d9c664e8397fcea614f6537e2861843c16009a
CalvinH-Dev <hanisch.calvin@gmail.com>
2025-09-23 15:22:44 +0200

    fixed imports in app.html and deleted comments

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,14 @@
 import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-// import { collection, Firestore, onSnapshot } from "@angular/fire/firestore";
 import { Footer } from "./shared/footer/footer";
 import { Header } from "./shared/header/header";
-import { Sidebar } from "./shared/sidebar/sidebar";
-import { AddContact } from "./main/contacts/add-contact/add-contact";
 
 @Component({
 	selector: "app-root",
-	imports: [RouterOutlet, Sidebar, Header, Footer, AddContact],
+	imports: [RouterOutlet, Header, Footer],
 	templateUrl: "./app.html",
 	styleUrl: "./app.scss",
 })
 export class App {
 	protected readonly title = signal("join");
-
-	//  db = inject(Firestore);
-
-	constructor() {
-		// const items = collection(this.db, "contacts");
-		// const snapshots = onSnapshot(items, (contact) => {
-		// 	contact.forEach((contacts) => {
-		// 		console.log(contacts.data());
-		// 	});
-		// });
-		// console.log(items);
-	}
 }

a934e2f016d8709f796ff20fd250656783f4babd
knostijr <christophkonst@icloud.com>
2025-09-29 11:33:24 +0200

    imported necessary components and basic structure of board-view

--- src/app/main/board/board-view/board-view.ts
+++ src/app/main/board/board-view/board-view.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, OnChanges, signal } from "@angular/core";
+import { Firestore } from "@angular/fire/firestore";
+import { Router } from "@angular/router";
+
+import { Button } from "@shared/components/button/button";
+import { SearchField } from "@shared/components/search-field/search-field";
+
+import { ToastAction } from "@shared/components/toast/toast";
+import { ToastService } from "@shared/services/toast.service";
 
 @Component({
   selector: 'app-board-view',
@@ -7,5 +15,15 @@ import { Component } from '@angular/core';
   styleUrl: './board-view.scss'
 })
 export class BoardView {
+
+  firestore = inject(Firestore);
+
+  router = inject(Router);
+
+  toastService = inject(ToastService);
+
+  id = input<string>("");
+
+  isAddTaskOverlayOpen = signal("");
 
 }
--- src/app/main/board/board.ts
+++ src/app/main/board/board.ts
@@ -1,9 +1,17 @@
-import { Component } from "@angular/core";
+import { CommonModule, ViewportScroller } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { BoardView } from "./board-view/board-view";
+import { TaskView } from "./task-view/task-view";
 
 @Component({
 	selector: "app-board",
-	imports: [],
+	imports: [CommonModule, BoardView, TaskView],
 	templateUrl: "./board.html",
 	styleUrl: "./board.scss",
 })
-export class Board {}
+export class Board {
+	vps = inject(ViewportScroller);
+	route = inject(ActivatedRoute);
+	router = inject(Router);
+}

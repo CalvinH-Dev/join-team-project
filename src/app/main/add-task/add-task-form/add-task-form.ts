6d88e822878bdb3c0002825360947c5a6879d51d
Madina Askar Zada <52121682+madina-aska@users.noreply.github.com>
2025-09-29 16:01:28 +0200

    Created add-task-form Component as child of add-task Component

---
+++ src/app/main/add-task/add-task-form/add-task-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-form',
+  imports: [],
+  templateUrl: './add-task-form.html',
+  styleUrl: './add-task-form.scss'
+})
+export class AddTaskForm {
+
+}

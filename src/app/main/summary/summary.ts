ff58e33bba112b743221d674214d46fe0115f1eb
Slin <210841740+Ph1L-s@users.noreply.github.com>
2025-10-08 11:26:02 +0200

    Examples for Task how to use them

--- src/app/main/summary/summary.ts
+++ src/app/main/summary/summary.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { Task } from "@core/interfaces/task";
+import { TaskService } from "@core/services/task-service";
 
 @Component({
 	selector: "app-summary",
@@ -6,4 +8,25 @@ import { Component } from "@angular/core";
 	templateUrl: "./summary.html",
 	styleUrl: "./summary.scss",
 })
-export class Summary {}
+export class Summary {
+	taskService = inject(TaskService);
+	items: Task[] = [];
+
+	constructor() {
+		const sub = this.taskService.getTasksAsObject().subscribe((tasks) => {
+			this.items = tasks;
+		});
+	}
+
+  get urgentItems() {
+    return this.items.filter(item => item.priority === 'urgent');
+  }
+
+  get categoryItems() {
+    return this.items.filter(item => item.category === 'Technical Task');
+  }
+
+  /* get categoryItems() {
+    return this.items.filter(item => item.category === 'Technical Task').slice(2, 3);
+  } */
+}

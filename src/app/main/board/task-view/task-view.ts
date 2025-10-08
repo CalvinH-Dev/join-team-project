49d20fd5766f05f716c8a9b3e6d4a841764e469b
Falko <162439151+hcplayer1988@users.noreply.github.com>
2025-10-08 10:52:37 +0200

    beginn with load task data

--- src/app/main/board/task-view/task-view.ts
+++ src/app/main/board/task-view/task-view.ts
@@ -1,20 +1,64 @@
-import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output, input } from '@angular/core';
-import { Button } from '@shared/components/button/button';
+import { CommonModule } from "@angular/common";
+import { Component, EventEmitter, OnInit, Output, inject, input } from "@angular/core";
+import { Firestore, doc, getDoc } from "@angular/fire/firestore";
+import { Contact } from "@core/interfaces/contact";
+import { Task } from "@core/interfaces/task";
+import { ContactService } from "@core/services/contact-service";
+import { TaskService } from "@core/services/task-service";
+import { Button } from "@shared/components/button/button";
 
 @Component({
 	selector: "app-task-view",
-	imports: [Button, CommonModule],
+	imports: [Button, CommonModule,],
 	templateUrl: "./task-view.html",
 	styleUrl: "./task-view.scss",
 })
-export class TaskView {
+export class TaskView implements OnInit {
+	firestore = inject(Firestore);
 
-  /**
-   * Input: Empfängt die ID der Task, die angezeigt werden soll.
-   * Das Hinzufügen dieser Zeile behebt den NG8002-Fehler in der board-view.html.
-   */
-  taskId = input.required<string>();
+	task: Task | null = null;
+
+	taskId = input<string>("");
+
+	contactService = inject(ContactService);
+  taskService = inject(TaskService);
+	assignedContacts: Contact[] = [];
+
+	ngOnInit() {
+		this.loadTask(this.taskId());
+		this.contactService.allContacts$.subscribe((contacts) => {
+			if (this.task?.assignedContacts) {
+				this.assignedContacts = contacts.filter((c) =>
+					this.task!.assignedContacts!.includes(c.id as string),
+				);
+			}
+		});
+	}
+
+	async loadTask(id: string) {
+		const taskRef = doc(this.firestore, `tasks/${id}`);
+		const snapshot = await getDoc(taskRef);
+
+		if (snapshot.exists()) {
+			const raw = snapshot.data() as any;
+
+			this.task = {
+				...raw,
+				dueDate: raw.dueDate?.toDate?.() ?? null,
+				createdAt: raw.createdAt?.toDate?.() ?? null,
+				updatedAt: raw.updatedAt?.toDate?.() ?? null,
+				subtasks:
+					raw.subtasks?.map((s: any) => ({
+						...s,
+						createdAt: s.createdAt?.toDate?.() ?? null,
+					})) ?? [],
+			};
+		} else {
+			console.warn(`Task with ID ${id} not found.`);
+			this.task = null;
+		}
+	}
+
 	/** Controls overlay visibility state */
 	isOverlayOpen = false;
 
@@ -32,6 +76,7 @@ export class TaskView {
 	openOverlay() {
 		this.open.emit();
 	}
+
 	onEditClick() {
 		this.edit.emit();
 	}
